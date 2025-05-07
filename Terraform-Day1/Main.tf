5a56007b4217dbebb7d7ac4b2ccbb535c7581b79
pravatkumar1004 <pravatkumar1004@gmail.com>
2025-05-07 13:19:09 +0530

    Update Main.tf

--- Terraform-Day1/Main.tf
+++ Terraform-Day1/Main.tf
@@ -1,18 +1,18 @@
-# resource "aws_instance" "terraformInstance" {
-#      ami = var.AIM
-#     instance_type = var.type
-#      tags = {
-#        Name="tarraform Instance"
-#      }
-# }
+ resource "aws_instance" "terraformInstance" {
+      ami = var.AIM
+     instance_type = var.type
+      tags = {
+        Name="tarraform Instance"
+      }
+ }
 
 
 
 
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "terraformbuckets-myproject-dev-20250423"
-  tags = {
-    Name="bocketForTerraform"
-  }
-}
+#resource "aws_s3_bucket" "bucket" {
+#  bucket = "terraformbuckets-myproject-dev-20250423"
+#  tags = {
+ #   Name="bocketForTerraform"
+#  }
+#}
